68ec1bba460dd34565242ba9158524374a6f3108
Dmitry <basher9696@gmail.com>
2016-02-11 22:02:25 +0200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   def edit
   end
 
+def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted."
